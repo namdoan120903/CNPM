222fd14e2da1b9a281246502c6f0c16c98052e61
namdoan120903 <doannambka@gmail.com>
2023-12-26 23:21:32 +0700

    add

--- src/main/java/com/example/qlsanbong/Controller/AdminController.java
+++ src/main/java/com/example/qlsanbong/Controller/AdminController.java
@@ -1,7 +1,5 @@
 package com.example.qlsanbong.Controller;
 
-import com.example.qlsanbong.DTO.DonHangDTO;
-import com.example.qlsanbong.DTO.SanBongDTO;
 import com.example.qlsanbong.Model.ChiTietDatSan;
 import com.example.qlsanbong.Model.ChiTietDonHang;
 import com.example.qlsanbong.Model.DonHang;
@@ -12,6 +10,7 @@ import com.example.qlsanbong.Service.AdminService;
 import com.example.qlsanbong.Service.NguoiDungService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/admin")
+@CrossOrigin("http://localhost:3000")
 public class AdminController {
   @Autowired
   AdminService adminService;
